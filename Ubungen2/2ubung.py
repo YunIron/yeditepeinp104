08eab98bda6a3455a3581689608b7ce1beaf44c9
Yunus Emre Özdemir <69750956+YunIron@users.noreply.github.com>
2025-04-28 17:56:10 +0300

    Add files via upload

---
+++ Ubungen2/2ubung.py
@@ -0,0 +1,8 @@
+x1 = int(input("Was ist erste X Koordinate? : "))
+y1 = int(input("Was ist erste Y Koordinate? : "))
+x2 = int(input("Was ist zweite X Koordinate? : "))
+y2 = int(input("Was ist zweite Y Koordinate? : "))
+
+Fazit = (x2-x1)**2 + (y2-y1)**2
+
+print(Fazit)
